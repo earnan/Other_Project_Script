2537ceea436d81557ced21348e21bc2b7879e69b
earnan <hi@arcsona.cn>
2022-04-07 14:40:36 +0800

    1

--- 377-1ssr筛选.py
+++ 377-1ssr筛选.py
@@ -86,7 +86,7 @@ for file_item in os.listdir(args.indir):
 
 
 # 分########################################################################
-# 查找共有ssr类型
+# 查找共有ssr的最小单元
 dict_total_unique = {}  # 各物种独有ssr
 list0 = Camellia_sinensis_L_O_Kuntze_cv_Xillian_1.keys()
 for access_id in list_species:
@@ -97,6 +97,7 @@ for access_id in list_species:
     tmp_list = set(createVar[access_id].keys())-set(list0)
     for i in tmp_list:
         dict_total_unique[access_id][i] = createVar[access_id][i]
+ic(dict_total_unique)
 # TODOdict_total_unique准备写入文件
 
 dict_total_share = {}  # 共有ssr
@@ -105,24 +106,22 @@ for i in list0:
 
 
 #总##################################################################################
-# 统计共有ssr不同物种中的重复
+# 统计共有ssr最小单元在不同物种中的重复次数
 for access_id in list_species:
     for i in list0:
         dict_total_share[i][access_id] = createVar[access_id][i]
-# ic(dict_total_share)
-
 
 #分################################################################################
 
 # 进一步统计
-dict_total_share_same = {}  # 在共有ssr中重复次数相同
+dict_total_share_same = {}  # 没有多态性的ssr类型及其重复次数
 dict_total_share_unique = {}  # 在共有ssr中重复次数不同
-for i in list0:
+for i in list0:  # ssr类型
     dict_total_share_unique[i] = {}
     list1 = dict_total_share[i]['Camellia_sinensis_L_O_Kuntze_cv_Xillian_1']
-    for access_id in list_species:
+    for access_id in list_species:  # 物种
         list1 = find(list1, dict_total_share[i][access_id])
-    dict_total_share_same[i] = list1  # 没有多态性的ssr类型及其重复次数
+    dict_total_share_same[i] = list1
 
     # for access_id in list_species:
 
